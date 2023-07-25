89dd4fe13bd36591261e9b4334659cfc6090f45c
phucduongminh <phucbopbnation@gmail.com>
2023-07-25 14:18:42 +0700

    fix udp

--- ESPServer/ESPServer.ino
+++ ESPServer/ESPServer.ino
@@ -23,23 +23,48 @@ void setup(){
     Serial.println(WiFi.localIP());
 
     UDP.begin(UDP_PORT);
-    Serial.print("Listening on UDP port");
+    Serial.print("Listening on UDP port: ");
     Serial.println(UDP_PORT);
 }
 
-void loop(){
+void startUdpServer() {
+  UDP.stop();
+  UDP.begin(UDP_PORT);
+  Serial.print("Listening on UDP port: ");
+  Serial.println(UDP_PORT);
+}
+
+void handleMessage(const char* message) {
+  if (strcmp(message, "ESP-ACK") == 0) {
+    // Close old UDP socket server and begin new
+    UDP.beginPacket(UDP.remoteIP(), UDP.remotePort());
+    UDP.print(WiFi.localIP());
+    UDP.endPacket();
+  } else if (strcmp(message, "CANCEL") == 0) {
+    // Handle CANCEL message (if needed)
+    // ...
+  } else if (strcmp(message, "SEND") == 0) {
+    // Handle SEND message (if needed)
+    // ...
+  } else if (strcmp(message, "RECEIVE") == 0) {
+    // Handle RECEIVE message (if needed)
+    // ...
+  } else {
+    // Unknown message type, handle appropriately (if needed)
+    // ...
+  }
+}
+
+void loop() {
   // If packet received...
   int packetSize = UDP.parsePacket();
   if (packetSize) {
     Serial.print("Received packet! Size: ");
     Serial.println(packetSize); 
-    int len = UDP.read(packet, 255);
-    if (len > 0)
-    {
-      packet[len] = '\0';
-    }
-    UDP.beginPacket(UDP.remoteIP(), UDP.remotePort());
-    UDP.print(WiFi.localIP());
-    UDP.endPacket();
+
+    int len = UDP.read(packet, sizeof(packet) - 1); // Leave space for null terminator
+    packet[len] = '\0'; // Null-terminate the received data
+    Serial.println(String(packet)); 
+    handleMessage(packet);
   }
 }
