7e58e9ab9678819946b96aa72bb279c0bfb46a12
phucduongminh <phucbopbnation@gmail.com>
2024-05-30 16:40:45 +0700

    done call api receive raw

--- test/TestReceiveRaw/TestReceiveRaw.ino
+++ test/TestReceiveRaw/TestReceiveRaw.ino
@@ -1,5 +1,3 @@
-#include "BaseOTA.h"
-
 #include <Arduino.h>
 #include <assert.h>
 #include <IRrecv.h>
@@ -7,44 +5,21 @@
 #include <IRac.h>
 #include <IRtext.h>
 #include <IRutils.h>
+#include <HTTPClient.h>
 
-// ==================== start of TUNEABLE PARAMETERS ====================
-// An IR detector/demodulator is connected to GPIO pin 14
-// e.g. D5 on a NodeMCU board.
-// Note: GPIO 16 won't work on the ESP8266 as it does not have interrupts.
-// Note: GPIO 14 won't work on the ESP32-C3 as it causes the board to reboot.
+const char *ssid = "Tue Minh Vlog";
+const char *password = "wifichongtrom";
+
 #ifdef ARDUINO_ESP32C3_DEV
 const uint16_t kRecvPin = 10;  // 14 on a ESP32-C3 causes a boot loop.
-#else  // ARDUINO_ESP32C3_DEV
+#else                          // ARDUINO_ESP32C3_DEV
 const uint16_t kRecvPin = 14;
-#endif  // ARDUINO_ESP32C3_DEV
+#endif                         // ARDUINO_ESP32C3_DEV
 
-// The Serial connection baud rate.
-// i.e. Status message will be sent to the PC at this baud rate.
-// Try to avoid slow speeds like 9600, as you will miss messages and
-// cause other problems. 115200 (or faster) is recommended.
-// NOTE: Make sure you set your Serial Monitor to the same speed.
 const uint32_t kBaudRate = 115200;
 
-// As this program is a special purpose capture/decoder, let us use a larger
-// than normal buffer so we can handle Air Conditioner remote codes.
 const uint16_t kCaptureBufferSize = 1024;
 
-// kTimeout is the Nr. of milli-Seconds of no-more-data before we consider a
-// message ended.
-// This parameter is an interesting trade-off. The longer the timeout, the more
-// complex a message it can capture. e.g. Some device protocols will send
-// multiple message packets in quick succession, like Air Conditioner remotes.
-// Air Coniditioner protocols often have a considerable gap (20-40+ms) between
-// packets.
-// The downside of a large timeout value is a lot of less complex protocols
-// send multiple messages when the remote's button is held down. The gap between
-// them is often also around 20+ms. This can result in the raw data be 2-3+
-// times larger than needed as it has captured 2-3+ messages in a single
-// capture. Setting a low timeout value can resolve this.
-// So, choosing the best kTimeout value for your use particular case is
-// quite nuanced. Good luck and happy hunting.
-// NOTE: Don't exceed kMaxTimeoutMs. Typically 130ms.
 #if DECODE_AC
 // Some A/C units have gaps in their protocols of ~40ms. e.g. Kelvinator
 // A value this large may swallow repeats of some protocols
@@ -53,41 +28,9 @@ const uint8_t kTimeout = 50;
 // Suits most messages, while not swallowing many repeats.
 const uint8_t kTimeout = 15;
 #endif  // DECODE_AC
-// Alternatives:
-// const uint8_t kTimeout = 90;
-// Suits messages with big gaps like XMP-1 & some aircon units, but can
-// accidentally swallow repeated messages in the rawData[] output.
-//
-// const uint8_t kTimeout = kMaxTimeoutMs;
-// This will set it to our currently allowed maximum.
-// Values this high are problematic because it is roughly the typical boundary
-// where most messages repeat.
-// e.g. It will stop decoding a message and start sending it to serial at
-//      precisely the time when the next message is likely to be transmitted,
-//      and may miss it.
 
-// Set the smallest sized "UNKNOWN" message packets we actually care about.
-// This value helps reduce the false-positive detection rate of IR background
-// noise as real messages. The chances of background IR noise getting detected
-// as a message increases with the length of the kTimeout value. (See above)
-// The downside of setting this message too large is you can miss some valid
-// short messages for protocols that this library doesn't yet decode.
-//
-// Set higher if you get lots of random short UNKNOWN messages when nothing
-// should be sending a message.
-// Set lower if you are sure your setup is working, but it doesn't see messages
-// from your device. (e.g. Other IR remotes work.)
-// NOTE: Set this value very high to effectively turn off UNKNOWN detection.
 const uint16_t kMinUnknownSize = 12;
 
-// How much percentage lee way do we give to incoming signals in order to match
-// it?
-// e.g. +/- 25% (default) to an expected value of 500 would mean matching a
-//      value between 375 & 625 inclusive.
-// Note: Default is 25(%). Going to a value >= 50(%) will cause some protocols
-//       to no longer match correctly. In normal situations you probably do not
-//       need to adjust this value. Typically that's when the library detects
-//       your remote's message some of the time, but not all of the time.
 const uint8_t kTolerancePercentage = kTolerance;  // kTolerance is normally 25%
 
 // Legacy (No longer supported!)
@@ -100,14 +43,56 @@ const uint8_t kTolerancePercentage = kTolerance;  // kTolerance is normally 25%
 IRrecv irrecv(15, kCaptureBufferSize, kTimeout, true);
 decode_results results;  // Somewhere to store the results
 
+//Raw To String
+String rawDataToString(uint16_t* rawData, uint16_t length) {
+  String str = "{";
+  for (uint16_t i = 0; i < length; i++) {
+    str += String(rawData[i]);
+    if (i < length - 1) {
+      str += ", ";
+    }
+  }
+  str += "}";
+  return str;
+}
+
+void callAPI(const String& baseURL, const String& button_id, const String& device_id, int rawdata_length, const String& rawdata) {
+  HTTPClient http;
+
+  String route = "/api/signal/learns/new";
+
+  http.begin(baseURL + route); 
+  http.addHeader("Content-Type", "application/json");
+
+  // Create the JSON payload
+  String payload = "{";
+  payload += "\"button_id\":\"" + button_id + "\",";
+  payload += "\"device_id\":\"" + device_id + "\",";
+  payload += "\"rawdata_length\":" + String(rawdata_length) + ",";
+  payload += "\"rawdata\":\"" + rawdata + "\"";
+  payload += "}";
+
+  int httpResponseCode = http.POST(payload);  // Send the POST request
+
+  if (httpResponseCode>0) {
+    String response = http.getString();
+    Serial.println(httpResponseCode);   // Print the response code
+    Serial.println(response);           // Print the response body
+  } else {
+    Serial.printf("Error on sending POST to route %s: ", route.c_str());
+    Serial.println(httpResponseCode);
+  }
+
+  http.end();
+}
+
 // This section of code runs only once at start-up.
 void setup() {
-  OTAwifi();  // start default wifi (previously saved on the ESP) for OTA
 #if defined(ESP8266)
   Serial.begin(kBaudRate, SERIAL_8N1, SERIAL_TX_ONLY);
-#else  // ESP8266
+#else              // ESP8266
   Serial.begin(kBaudRate, SERIAL_8N1);
-#endif  // ESP8266
+#endif             // ESP8266
   while (!Serial)  // Wait for the serial connection to be establised.
     delay(50);
   // Perform a low level sanity checks that the compiler performs bit field
@@ -115,78 +100,41 @@ void setup() {
   assert(irutils::lowLevelSanityCheck() == 0);
 
   Serial.printf("\n" D_STR_IRRECVDUMP_STARTUP "\n", kRecvPin);
-  OTAinit();  // setup OTA handlers and show IP
 #if DECODE_HASH
   // Ignore messages with less than minimum on or off pulses.
   irrecv.setUnknownThreshold(kMinUnknownSize);
-#endif  // DECODE_HASH
+#endif                                        // DECODE_HASH
   irrecv.setTolerance(kTolerancePercentage);  // Override the default tolerance.
-  irrecv.enableIRIn();  // Start the receiver
+  irrecv.enableIRIn();                        // Start the receiver
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  { // Check for the connection
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  Serial.println("Connected to the WiFi network");
 }
 
 // The repeating section of the code
 void loop() {
   // Check if the IR code has been received.
+  static int signalCounter = 0;  // Counter for received signals
   if (irrecv.decode(&results)) {
-    // Display a crude timestamp.
-    uint32_t now = millis();
-    // D_STR_TIMESTAMP "Timestamp"
-    Serial.printf(D_STR_TIMESTAMP " : %06u.%03u\n", now / 1000, now % 1000);
+     signalCounter++;
+    Serial.println();
+    if (signalCounter == 2) {
+      uint16_t* rawData = resultToRawData(&results);
+      const uint16_t length = getCorrectedRawLength(&results);
+      String str = rawDataToString(rawData, length);
+      Serial.printf("rawData[%d] = %s\n", (int)length, str.c_str());  // Print the raw data
+      callAPI("http://192.168.1.39:3001", "power", "myairphucdu", (int)length, str);
+      delete[] rawData;
 
-    // Check if we got an IR message that was to big for our capture buffer.
-    // D_WARN_BUFFERFULL \ "WARNING: IR code is too big for buffer (>= %d). " \ "This result shouldn't be trusted until this is resolved. " \"Edit & increase `kCaptureBufferSize`."
-    if (results.overflow)
-      Serial.printf(D_WARN_BUFFERFULL "\n", kCaptureBufferSize);
-
-    // Display the library version the message was captured with.
-    // D_STR_LIBRARY "Library"
-    Serial.println(D_STR_LIBRARY "   : v" _IRREMOTEESP8266_VERSION_STR "\n");
-
-    // Display the tolerance percentage if it has been change from the default.
-    // D_STR_TOLERANCE "Tolerance"
-    if (kTolerancePercentage != kTolerance)
-      Serial.printf(D_STR_TOLERANCE " : %d%%\n", kTolerancePercentage);
-
-    // Display the basic output of what we found.
-    Serial.print(resultToHumanReadableBasic(&results));
-    // Display any extra A/C info if we have it.
-    String description = IRAcUtils::resultAcToString(&results);
-
-    // D_STR_MESGDESC "Mesg Desc."
-    if (description.length()) Serial.println(D_STR_MESGDESC ": " + description);
-    yield();  // Feed the WDT as the text output can take a while to print.
-#if LEGACY_TIMING_INFO
-    // Output legacy RAW timing info of the result.
-    Serial.println(resultToTimingInfo(&results));
-    yield();  // Feed the WDT (again)
-#endif  // LEGACY_TIMING_INFO
-    // Output the results as source code
-    Serial.println(resultToSourceCode(&results)); // That what we need
-    Serial.println();    // Blank line between entries
-    yield();             // Feed the WDT (again)
-  }
-  OTAloopHandler();
-}
-
-// This code from IRuntils.h is used in this example.
-/* output += F("] = {");  // Start declaration
-
-  // Dump data
-  for (uint16_t i = 1; i < results->rawlen; i++) {
-    uint32_t usecs;
-    for (usecs = results->rawbuf[i] * kRawTick; usecs > UINT16_MAX;
-         usecs -= UINT16_MAX) {
-      output += uint64ToString(UINT16_MAX);
-      if (i % 2)
-        output += F(", 0,  ");
-      else
-        output += F(",  0, ");
+      signalCounter = 0;  // Reset the counter
     }
-    output += uint64ToString(usecs, 10);
-    if (i < results->rawlen - 1)
-      output += kCommaSpaceStr;            // ',' not needed on the last one
-    if (i % 2 == 0) output += ' ';  // Extra if it was even.
+    Serial.println();  // Blank line between entries
+    yield();           // Feed the WDT (again)
   }
-
-  // End declaration
-  output += F("};"); */
+}
